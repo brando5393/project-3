0a5d5344ba4ef22150356501c6f350dc60d7cf38
Brandon Williams <brando5393@gmail.com>
2020-04-25 14:02:45 -0500

    added starter code for routes

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -4,4 +4,20 @@ const db = mongoose.connection;
 
 module.exports = ((router) =>{
     router.get("/", (req, res) => res.sendFile(path.join(__dirname, "../client/public/index.html")));
+    
+    router.get("/posts:category", (req, res) =>{
+        // get all posts from db that match a specific category
+    });
+    
+    router.get("/posts:id", (req, res) =>{
+        // get post from db that matches a specific id
+    });
+    
+    router.post("/posts", (req, res) =>{
+        // create post
+    });
+    
+    router.patch("/posts:id", (req, res) =>{
+        // update votes for specific post
+    });
 });
