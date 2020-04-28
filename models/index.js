4a460ff9f7c234dcf6d15d52af08ee165a2652a7
Dylan Myers <dylanmyers178@gmail.com>
2020-04-27 20:58:40 -0500

    fixing keys

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 // Exporting an object containing all of our models
 
 module.exports = {
-    Article: require("./post"),
-    Note: require("./user")
+    Article: require("./Post"),
+    Note: require("./User")
   };
   
