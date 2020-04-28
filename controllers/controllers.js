de82dc410c4cafbf038bab901112881119304f79
Dylan Myers <dylanmyers178@gmail.com>
2020-04-27 21:16:11 -0500

    adding catagory to Post file

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,7 +1,7 @@
-// const router = express.router();
-// const path = require('path');
-// const db = mongoose.connection;
+const router = express.router();
+const path = require('path');
+const db = mongoose.connection;
 
-// module.exports = ((router) =>{
-//     router.get("/", (req, res) => res.sendFile(path.join(__dirname, "../client/public/index.html")));
-// });
+module.exports = ((router) =>{
+    router.get("/", (req, res) => res.sendFile(path.join(__dirname, "../client/public/index.html")));
+});
