f104a7e76fa6e8b8ec3fffd5ee35e310f91e3702
Brandon Williams <brando5393@gmail.com>
2020-04-29 20:13:39 -0500

    can pull data from db [dylan assisted via live share]

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,7 +1,74 @@
-// const router = express.router();
-// const path = require('path');
-// const db = mongoose.connection;
+const express = require('express');
+const router = require('express').Router();
+const path = require('path');
+const mongoose = require('mongoose');
+const models = require('../models');
+    router.get("/", (req, res) => res.sendFile(path.join(__dirname, "../client/public/index.html")));
 
-// module.exports = ((router) =>{
-//     router.get("/", (req, res) => res.sendFile(path.join(__dirname, "../client/public/index.html")));
-// });
+    // pull all posts
+    router.get("/api/posts", (req, res) =>{
+        models.Post.find((err, posts) =>{
+            if(err){
+                throw err;
+            }else{
+                // res.redirect(200, '/posts');
+                // res.send('Hello World');
+                res.json(posts);
+            }
+        });
+    });
+// Get all posts in a given category
+    router.get("/api/posts/:category", (req, res) =>{
+        models.Post.find((err, posts) =>{
+            if(err){
+                throw err;
+            }else{
+                
+            }
+        });
+    });
+
+// Get single post in category 
+    router.get("/api/posts/:category/:id", (req, res) =>{
+        models.Post.find((err, posts) =>{
+            if(err){
+                throw err;
+            }else{
+                // res.redirect(200, '/posts');
+                // res.send('Hello World');
+                res.json(posts);
+            }
+        });
+    });
+
+// Create Post
+    // Get Post Catagory 
+    router.post("/api/posts/", (req, res) =>{
+        console.log(req.body);
+        models.Post.insert(req.body, (err, saved) =>{
+            if(err){
+                throw err;
+            }else{
+                // res.redirect(200, '/posts');
+                // res.send('Hello World');
+                res.json(saved);
+
+                // const postTemplate = {
+                //     title: this.title,
+                //     author: this.author,
+                //     category: this.category,
+                //     body: this.body
+                // }
+            }
+        });
+    });
+
+
+
+    
+
+
+    router.get('*', (req, res) =>{
+        res.sendFile(path.join(__dirname, "../client/public/index.html"));
+    })
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
 const express = require('express');
 const app =  express();
+const routes = require('./controllers/controllers')
 const PORT = process.env.PORT || 5000;
 require('dotenv').config();
 const mongoose = require("mongoose");
@@ -9,34 +9,11 @@ const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/devforum";
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('./client/public'));
-
-
-require("./controllers.js")(router);
+app.use(routes);
 
 mongoose.connect(MONGODB_URI);
 
 app.listen(PORT, (err) =>{
     if(err) throw err;
     console.log(`Server started on port ${PORT}`);
-=======
-const express = require('express');
-const app =  express();
-const PORT = process.env.PORT || 5000;
-require('dotenv').config();
-const mongoose = require("mongoose");
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/devforum";
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('./client/public'));
-
-
-//require("./controllers.js")(router);
-
-mongoose.connect(MONGODB_URI);
-
-app.listen(PORT, (err) =>{
-    if(err) throw err;
-    console.log(`Server started on port ${PORT}`);
->>>>>>> 4a460ff9f7c234dcf6d15d52af08ee165a2652a7
 });
