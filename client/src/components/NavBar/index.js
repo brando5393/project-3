71982f1d580d84da4da86ebd9fe3858b02655d6e
klhelker <57188023+klhelker@users.noreply.github.com>
2020-05-04 20:54:45 -0500

    fixed navbar styles

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -2,47 +2,41 @@ import React from "react";
 
 function NavBar(props) {
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <a
-          href="#home"
-          onClick={() => props.handlePageChange("Home")}
-          className={props.currentPage === "Home" ? "nav-link active" : "nav-link"}
-        >
-          Home
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#categories"
-          onClick={() => props.handlePageChange("Categories")}
-          className={props.currentPage === "Categories" ? "nav-link active" : "nav-link"}
-        >
-          Categories
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#create-post"
-          onClick={() => props.handlePageChange("Create-post")}
-          className={props.currentPage === "Create-Post" ? "nav-link active" : "nav-link"}
-        >
-          Create Post
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#Sign-In"
-          onClick={() => props.handlePageChange("Sign-In")}
-          className={props.currentPage === "Sign-In" ? "nav-link active" : "nav-link"}
-        >
-          Sign In
-        </a>
-      </li>
-          
-        
-      
-    </ul>
+  <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a className="navbar-brand" href="/">Dev Forum 2.0</a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> Menu
+        <span className="navbar-toggler-icon"></span>
+    </button>
+
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+                <a className="nav-link" href="/post">HTML</a>
+            </li>
+            <li className="nav-item">
+                    <a className="nav-link" href="/post">CSS</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="/post">JavaScript</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="/post">Node</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="/post">React</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="/post">Databases</a>
+            </li>
+        </ul>
+        <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+                <a className="nav-link" href="/form">Create Post</a>
+            </li>
+        </ul>
+    </div>
+  </nav>
   );
 }
 
