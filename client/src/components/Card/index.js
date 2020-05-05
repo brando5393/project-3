2681dcd9233777d62ec42ab16421c3d091b35c19
klhelker <57188023+klhelker@users.noreply.github.com>
2020-05-04 20:35:24 -0500

    added href submit button

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -9,8 +9,8 @@
               <img src= {props.image} className="card-img-top" alt= {props.title} />
               <div className="card-body">
               <h5 className="card-title">{props.heading}</h5>
-              <p className="card-text">{props.discription}</p>
-                
+              <p className="card-text">{props.description}</p>
+              <a href="#" class="btn btn-primary enter-btn" value={props.heading}>Enter Forum</a>
               </div>
             </div>
           </div>
