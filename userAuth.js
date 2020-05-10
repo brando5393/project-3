5d162c24791cc4decf5995370e6b170652e94ca4
Brandon Williams <brando5393@gmail.com>
2020-05-10 12:07:43 -0500

    added bcrypt code to check passwords on sign in

--- userAuth.js
+++ userAuth.js
@@ -5,12 +5,26 @@ const models = require('./models/index');
 const hashPassword = password =>{
     bcrypt.genSalt(10, (err, salt) =>{
         bcrypt.hash(password, salt, (err, hash) =>{
-            // send hashed password to db
+            if(err){
+                throw err;
+            }else{
+                // send hashed password to db
+            };
         });
     });
 };
 
-module.exports = {
-    hashPassword: hashPassword
+const checkPassword = password =>{
+    bcrypt.compare(password, hash, (err, result) => {
+        if(err){
+            throw err;
+        }else{
+            // compare password and hash if match login else reject
+        };
+    });
+};
 
+module.exports = {
+    hashPassword: hashPassword,
+    checkPassword: checkPassword
 }
