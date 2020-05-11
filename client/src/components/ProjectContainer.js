9055c7978e9fe9c915d56987cdba57e585732be9
klhelker <57188023+klhelker@users.noreply.github.com>
2020-05-11 07:53:52 -0500

    adding functionality to forum page

--- client/src/components/ProjectContainer.js
+++ client/src/components/ProjectContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Forum from "../pages/Forum"
 import Home from "../pages/Home";
 import CreatePost from "../components/CreatePost"
-import {Route, Link, Switch} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 
 
 class ProjectContainer extends Component {
