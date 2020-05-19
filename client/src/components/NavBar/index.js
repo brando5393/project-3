eb066393fde63dc495944ca4df87cbc4e44ae5f4
klhelker <57188023+klhelker@users.noreply.github.com>
2020-05-18 19:08:06 -0500

    fixed navbar css

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -14,22 +14,22 @@ function NavBar(props) {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-                <a className="nav-link" data-category= "HTML" href="/posts/HTML">HTML</a>
+                <a className="nav-link" data-category= "HTML" href="/posts">HTML</a>
             </li>
             <li className="nav-item">
-                    <a className="nav-link" data-category= "CSS" href="/posts/CSS">CSS</a>
+                    <a className="nav-link" data-category= "CSS" href="/posts">CSS</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link" data-category= "Javascript" href="/posts/JavaScript">JavaScript</a>
+                <a className="nav-link" data-category= "Javascript" href="/posts">JavaScript</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link" data-category = "Node" href="/posts/Node">Node</a>
+                <a className="nav-link" data-category = "Node" href="/posts">Node</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link" data-category = "React" href="/posts/React">React</a>
+                <a className="nav-link" data-category = "React" href="/posts">React</a>
             </li>
             <li className="nav-item">
-                <a className="nav-link" data-category = "Databases" href="/posts/Databases">Databases</a>
+                <a className="nav-link" data-category = "Databases" href="/posts">Databases</a>
             </li>
             <li className="nav-item">
                 <Link className="nav-link" to ="/createposts">Create Post</Link>
